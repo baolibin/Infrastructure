7c8f9bde4d339b200beb642f0e08505efbdbd379
baolibin <2578858653@qq.com>
2018-08-25 21:49:21 +0800

    rpc

---
+++ src/main/java/com/libin/rpc/RpcHello.java
@@ -0,0 +1,11 @@
+package com.libin.rpc;
+
+/**
+ * Copyright (c) 2018/8/25. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p/>
+ * Purpose :
+ */
+
+public class RpcHello {
+}
